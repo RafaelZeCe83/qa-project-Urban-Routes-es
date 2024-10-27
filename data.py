ad618b1f1ee7aafea598604775aaa04ae3fc7537
Rafael Zermeño <razc83@hotmail.com>
2024-10-27 07:55:52 -0700

    Proyecto Sp8 RZCQA14

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-urban_routes_url = 'https://cnt-4a02dbcd-4b1c-4664-b595-975b1ee6d7cc.containerhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://cnt-77af5721-7744-4dec-a063-35e15e0f1776.containerhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
